0a6d6a2c687024a7d35cd93766fbb4fe16f1e935
Eugene Berezin <berezin.nlp@gmail.com>
2020-03-05 08:53:47 -0800

    commit

--- GitHubJobsApiCourse/GitHubJobsApiCourse/MainViewController.swift
+++ GitHubJobsApiCourse/GitHubJobsApiCourse/MainViewController.swift
@@ -19,6 +19,8 @@ class MainViewController: UICollectionViewController, UISearchBarDelegate {
         collectionView.backgroundColor = UIColor(named: "backgroundMain")
         collectionView.register(SearchViewCell.self, forCellWithReuseIdentifier: cellId)
         setupSearchBar()
+        
+        
         Service.shared.getResults(description: "Software Developer", location: "San Francisco") { result in
             switch result {
             case .success(let results):
